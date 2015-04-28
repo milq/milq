3401eceb1adc4380b738e3b2f150b605aa858aa0
Manuel Ignacio López Quintero <milqmail@gmail.com>
2015-04-28 15:19:15 +0200

    Update just-after-installing-ubuntu-debian.sh

--- just-after-installing-ubuntu-debian.sh
+++ just-after-installing-ubuntu-debian.sh
@@ -1,3 +1,12 @@
+# Development tools:
+sudo apt-get install -y gdebi
+
+# Development tools:
+sudo apt-get install -y build-essential cmake geany 
+
+# Qt 5:
+qt5-default qtcreator -y qtdeclarative5-qtquick2-plugin
+
 # Useful terminal utilities:
 sudo apt-get install -y htop lshw p7zip pdftk
 
