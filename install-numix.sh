36f63ee1deda2034145b9a12873c2c930043ae21
Manuel Ignacio López Quintero <milqmail@gmail.com>
2015-05-11 16:25:12 +0200

    Update install-numix.sh

--- install-numix.sh
+++ install-numix.sh
@@ -1,1 +1,18 @@
+#!/bin/sh
 
+cd /usr/share/themes/
+sudo rm -rf Numix
+sudo git clone https://github.com/shimmerproject/Numix.git
+
+cd /usr/share/icons/
+sudo rm -rf numix-icon-theme numix-icon-theme-circle Numix Numix-Light Numix-Circle Numix-Circle-Light
+
+sudo git clone https://github.com/numixproject/numix-icon-theme.git
+sudo mv numix-icon-theme/Numix ./
+sudo mv numix-icon-theme/Numix-Light ./
+
+sudo git clone https://github.com/numixproject/numix-icon-theme-circle.git
+sudo mv numix-icon-theme-circle/Numix-Circle ./
+sudo mv numix-icon-theme-circle/Numix-Circle-Light ./
+
+sudo rm -rf numix-icon-theme numix-icon-theme-circle
