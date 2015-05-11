cec510045f815daec863ca3934b43ca4f43b5e14
Manuel Ignacio López Quintero <milqmail@gmail.com>
2015-05-11 16:36:38 +0200

    Update install-python-scientific-computing-ubuntu-debian.sh

--- install-python-scientific-computing-ubuntu-debian.sh
+++ install-python-scientific-computing-ubuntu-debian.sh
@@ -7,19 +7,21 @@ sudo apt-get -y dist-upgrade
 sudo apt-get -y autoremove
 
 # Interpreter and the package manager:
-sudo apt-get -y install python3 python3-pip
+sudo apt-get install -y python3 python3-pip
 
 # Essential scientific libraries:
-sudo apt-get install python3-numpy python3-matplotlib python3-scipy python3-pandas python3-simpy
+sudo apt-get install -y python3-numpy python3-matplotlib python3-scipy python3-pandas python3-simpy
 
 # IPython:
-sudo apt-get install ipython3 ipython3-notebook
+sudo apt-get install -y ipython3 ipython3-notebook
 
 # OpenGL:
-sudo apt-get install python3-opengl
+sudo apt-get install -y python3-opengl
 
 # GUI development:
-sudo apt-get install python3-pyqt5 python3-pyqt5.qtopengl python3-pyqt5.qtquick
+sudo apt-get install -y python3-pyqt5 python3-pyqt5.qtopengl python3-pyqt5.qtquick
 
 # Data storage:
-sudo apt-get install python3-h5py
+sudo apt-get install -y python3-h5py
+
+
