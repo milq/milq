f043414dfaf69310dfaf1c61c3d27d85354ded6e
Manuel Ignacio López Quintero <milqmail@gmail.com>
2015-05-11 17:13:46 +0200

    Update install-numix.sh

--- install-numix.sh
+++ install-numix.sh
@@ -19,3 +19,8 @@ sudo mv numix-icon-theme-circle/Numix-Circle ./
 sudo mv numix-icon-theme-circle/Numix-Circle-Light ./
 
 sudo rm -rf numix-icon-theme numix-icon-theme-circle
+
+# Activate Numix (theme, WM, and icons) in XFCE (comment these lines if you do not use Xfce):
+xfconf-query -c xsettings -p /Net/ThemeName -s "Numix"
+xfconf-query -c xfwm4 -p /general/theme -s "Numix"
+xfconf-query -c xsettings -p /Net/IconThemeName -s "Numix-Circle"
