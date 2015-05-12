aa7f6d144a159619ab516dbd5f775f424bef66ef
Manuel Ignacio López Quintero <milqmail@gmail.com>
2015-05-12 10:52:03 +0200

    Update just-after-installing-ubuntu-debian.sh

--- just-after-installing-ubuntu-debian.sh
+++ just-after-installing-ubuntu-debian.sh
@@ -44,4 +44,4 @@ sudo apt-get install -y libav-tools vlc
 # sudo apt-get install -y texlive-full texmaker
 
 # If you use Xfce, it is recommended to install this application launcher 
-# sudo apt-get install -y xfce4-whiskermenu-plugin
+# sudo apt-get install -y xfce4-whiskermenu-plugin gtk-theme-config
