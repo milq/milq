0c40fce4aee46a86b90cb2ad3a55e2df9c9e3a80
Manuel Ignacio López Quintero <milqmail@gmail.com>
2015-05-13 11:57:44 +0200

    Create install-opencv-ubuntu-debian.sh

---
+++ install-opencv-ubuntu-debian.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# INSTALL THE DEPENDENCIES
+
+# Build tools:
+sudo apt-get install -y build-essential cmake
+
+# GUI:
+sudo apt-get install -y qt5-default libvtk6-dev
+
+# Media I/O:
+sudo apt-get install -y zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev libjasper-dev libopenexr-dev libgdal-dev
+
+# Video I/O:
+sudo apt-get install -y libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev
+
+# Optional video I/O non-free library (uncomment next line to install it):
+# sudo apt-get install -y libfaac-dev
+
+# Parallelism and linear algebra libraries:
+sudo apt-get install -y libtbb-dev libeigen3-dev
+
+# Python:
+sudo apt-get install -y python-dev python-tk python-numpy python3-dev python3-tk python3-numpy
+
+# Java:
+sudo apt-get install -y ant default-jdk
+
+# Documentation:
+sudo apt-get install -y doxygen sphinx-common texlive-latex-extra
+
