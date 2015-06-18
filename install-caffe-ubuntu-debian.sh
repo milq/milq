a7622ce448facf22adc07a4f6f0031f282054015
Manuel Ignacio López Quintero <milqmail@gmail.com>
2015-06-18 12:52:24 +0200

    Create install-caffe-ubuntu-debian.sh

---
+++ install-caffe-ubuntu-debian.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# CAFFE INSTALLATION: http://caffe.berkeleyvision.org/installation.html
+
+sudo apt-get install -y libopenblas-dev
+sudo apt-get install -y libboost-all-dev
+sudo apt-get install -y libprotobuf-dev
