dadffc646d2d7aa9bdc7e191762bf6d70ecd0215
Manuel Ignacio López Quintero <milqmail@gmail.com>
2015-06-18 13:40:49 +0200

    Update install-caffe-ubuntu-debian.sh

--- install-caffe-ubuntu-debian.sh
+++ install-caffe-ubuntu-debian.sh
@@ -3,5 +3,16 @@
 # CAFFE INSTALLATION: http://caffe.berkeleyvision.org/installation.html
 
 sudo apt-get install -y libopenblas-dev
+
 sudo apt-get install -y libboost-all-dev
+
+# OPENCV
+
 sudo apt-get install -y libprotobuf-dev
+sudo apt-get install -y libgflags-dev
+sudo apt-get install -y libgoogle-glog-dev 
+
+sudo apt-get install -y libhdf5-dev
+sudo apt-get install -y libleveldb-dev
+sudo apt-get install -y liblmdb-dev
+sudo apt-get install -y libsnappy-dev
