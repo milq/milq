cc7f9c3e10f0ca97295a2bb91a68fbb228fa709c
Manuel Ignacio López Quintero <milqmail@gmail.com>
2015-06-18 15:58:39 +0200

    Update install-caffe-ubuntu-debian.sh

--- install-caffe-ubuntu-debian.sh
+++ install-caffe-ubuntu-debian.sh
@@ -27,9 +27,19 @@ sudo apt-get install -y liblmdb-dev
 sudo apt-get install -y python3-dev python3-numpy libboost-python-dev
 
 git clone https://github.com/BVLC/caffe.git
+
 cd caffe
-mkdir build
-cd build
-cmake -DCPU_ONLY=ON -DUSE_CUDNN=ON ..
-make all
-make runtest
+
+# In 'caffe/cmake/Dependencies.cmake' set BLAS to "Open".
+# In 'caffe/CMakeLists.txt' set 'python_version' to "3".
+
+# mkdir build
+# cd build
+# cmake -DCPU_ONLY=ON -DUSE_CUDNN=OFF ..
+
+
+# If you see during the CMake this: 'pthread_create - not found' do the following:
+# sudo nano /usr/share/cmake-2.8/Modules/CheckIncludeFiles.cmake
+# In the line: "${CMAKE_CONFIGURABLE_FILE_CONTENT}\n\nint main(){return 0;}\n")
+# Change int main() to int main(void).
+# URL: http://stackoverflow.com/questions/24813827/cmake-failing-to-detect-pthreads-due-to-warnings/25130590
