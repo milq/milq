261a2095aa8c01688370d8ab1e1d592a1fe37202
Manuel Ignacio López Quintero <miloqu@gmail.com>
2015-11-24 12:48:09 +0100

    Update install-caffe.sh

--- install-caffe.sh
+++ install-caffe.sh
@@ -13,16 +13,16 @@ sudo apt-get install -y libopenblas-dev
 
 sudo apt-get install -y libboost-all-dev
 
-# Install OpenCV (http://milq.github.io/install-opencv-ubuntu-debian).
-
 sudo apt-get install -y libprotobuf-dev protobuf-compiler
 sudo apt-get install -y libgoogle-glog-dev
 sudo apt-get install -y libgflags-dev
-
 sudo apt-get install -y libhdf5-dev
+
+# Install OpenCV (http://milq.github.io/install-opencv-ubuntu-debian).
+
+sudo apt-get install -y liblmdb-dev
 sudo apt-get install -y libleveldb-dev
 sudo apt-get install -y libsnappy-dev
-sudo apt-get install -y liblmdb-dev
 
 # Python 3 
 sudo apt-get install -y python3-dev python3-numpy libboost-python-dev
