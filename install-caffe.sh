b2632ef3dded2bfe62c22b2b4519feff2b418c0b
Manuel Ignacio López Quintero <miloqu@gmail.com>
2015-11-27 14:51:38 +0100

    Update install-caffe.sh

--- install-caffe.sh
+++ install-caffe.sh
@@ -27,20 +27,12 @@ sudo apt-get install -y python3-dev python3-numpy libboost-python-dev
 git clone https://github.com/BVLC/caffe.git
 cd caffe
 
-# In 'caffe/CMakeLists.txt' set 'python_version' to "3".
-# In 'caffe/cmake/Dependencies.cmake' set BLAS to "Open" or set '-DBLAS=Open' in 'cmake'.
-# In 'caffe/cmake/Dependencies.cmake' add 'set(Python_ADDITIONAL_VERSIONS 3.4)'
-# before 'find_package(PythonLibs 3.0). URL: http://askubuntu.com/questions/479260/cmake-can-not-find-pythonlibs.
-
-# mkdir build
-# cd build
-# cmake -DBLAS=Open -DCPU_ONLY=ON -DUSE_CUDNN=OFF ..
-# make -j8
-# make install
-
-# 'CV_BGR2GRAY' errors In 'caffe/examples/cpp_classification/'
-# 'It seems, you are (accidentally) using the OpenCV 3.0(master) opencv branch.
-# A lot of constants have changed there, like most of the CV_ prefixes were changed to
-# cv:: namespace, CV_BGR2GRAY is now cv::COLOR_BGR2GRAY, etc.
-# TODO: Go to 'caffe/examples/cpp_classification/classification.cpp' and fix the errors.
-# URL: http://stackoverflow.com/questions/23922620/open-cv-not-loading-correctly.
+# cp Makefile.config.example Makefile.config
+# Adjust Makefile.config (for example, if using Anaconda Python)
+# make all -j8
+# make test -j8
+# make runtest
+#
+# make all matcaffe
+# make mattest
+# 
