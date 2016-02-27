72a8b4da8a8bfcb32890c54d8a50fb1ca3882718
Manuel Ignacio López Quintero <miloqu@gmail.com>
2016-02-27 20:55:16 +0100

    Update install-opencl-debian.sh

--- install-opencl-debian.sh
+++ install-opencl-debian.sh
@@ -14,3 +14,8 @@ sudo apt-get -t jessie-backports install beignet beignet-dev
 
 sudo updatedb
 locate libOpenCL.so
+
+
+# sudo apt-get install opencl-headers ocl-icd-dev ocl-icd-libopencl1    
+# sudo apt-get install clinfo    
+
