fcd96f4f15561e2258e64bc6dbee0e5236fcaaca
Manuel Ignacio López Quintero <miloqu@gmail.com>
2016-02-27 19:46:35 +0100

    Update install-opencl-debian.sh

--- install-opencl-debian.sh
+++ install-opencl-debian.sh
@@ -1,2 +1,16 @@
+# BACKPORTS PACKAGES
+# ------------------
+# Check that you have the backports packages in your Debian in '/etc/apt/sources.list'.
+# If not, to add backports packages, in an terminal run 'sudo nano /etc/apt/sources.list' and append the following line:
+# deb http://ftp.de.debian.org/debian jessie-backports main
+# Finally, in a terminal, execute: 'sudo apt-get update'
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get -y autoremove
+
+
+
 sudo updatedb
 locate libOpenCL.so
