ee11b77ab2b7e055bf3be44a7a76290f75a64040
Manuel Ignacio López Quintero <miloqu@gmail.com>
2016-06-16 17:22:40 +0200

    Update install-cli-software.sh

--- install-cli-software.sh
+++ install-cli-software.sh
@@ -7,13 +7,19 @@ sudo apt-get -y dist-upgrade
 sudo apt-get -y autoremove
 
 # Useful terminal utilities:
-sudo apt-get install -y faketime  p7zip p7zip-full pdftk screen unrar-free unzip wget
+sudo apt-get install -y faketime pdftk wget
 
 # Htop (interactive process viewer):
 sudo apt-get install -y htop
 
 # Hardware Lister (list hardware):
 sudo apt-get install -y lshw
+
+# File archivers
+sudo apt-get install -y p7zip p7zip-full unrar-free unzip
+
+# Terminal multiplexer
+sudo apt-get install -y screen
 
 # VCS:
 sudo apt-get install -y git subversion
