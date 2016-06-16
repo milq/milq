cacf37a1d8ab0135877aa62abef323d61b930a72
Manuel Ignacio López Quintero <miloqu@gmail.com>
2016-06-16 16:32:23 +0200

    Create install-cli-apps.sh

---
+++ install-cli-apps.sh
@@ -0,0 +1,7 @@
+# Script that installs command-line interface applications for Ubuntu/Debian
+
+# Keep Ubuntu or Debian up to date
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get -y autoremove
