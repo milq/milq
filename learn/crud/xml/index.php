7f3ff5fb029707bdc6f862cbb6b243f1b7524990
Manuel Ignacio López Quintero <miloqu@gmail.com>
2017-05-17 11:00:07 +0200

    Update index.php

--- learn/crud/xml/index.php
+++ learn/crud/xml/index.php
@@ -12,40 +12,37 @@
 
     <a href='form.php'>Insert a new student</a><br /><br />
 
-    <?php
+    <table title='student_table'>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>First name</th>
+          <th>Last name</th>
+          <th>Nickname</th>
+          <th>Date of birth</th>
+          <th>Mark</th>
+          <th>Edit</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+      <?php
+      require_once('load_xml.php');
 
-    require_once('load_xml.php');
-
-    echo "<table title='student_table'>";
-    echo '<thead>';
-    echo '<tr>';
-    echo '<th>ID</th>';
-    echo '<th>First name</th>';
-    echo '<th>Last name</th>';
-    echo '<th>Nickname</th>';
-    echo '<th>Date of birth</th>';
-    echo '<th>Mark</th>';
-    echo '<th>Edit</th>';
-    echo '<th>Delete</th>';
-    echo '</tr>';
-    echo '</thead>';
-    echo '<tbody>';
-
-    foreach($xml->children() as $student) {
-      echo '<tr>';
-      echo '<td>' . $student['id'] . '</td>';
-      echo '<td>' . $student->first_name . '</td>';
-      echo '<td>' . $student->last_name . '</td>';
-      echo '<td>' . $student->nickname . '</td>';
-      echo '<td>' . $student->date_of_birth . '</td>';
-      echo '<td>' . $student->mark . '</td>';
-      echo "<td><a title='edit_" . $student['id'] . "' href='form.php?id=" . $student['id'] . "'>Edit</a></td>";
-      echo "<td><a title='delete_" . $student['id'] . "' href='process.php?delete=yes&id=" . $student['id'] . "'>Delete</a></td>";
-      echo '</tr>';
-    }
-
-    echo '</tbody>';
-    echo '</table>';
-    ?>
+      foreach($xml->children() as $student) {
+        echo '<tr>';
+        echo '<td>' . $student['id'] . '</td>';
+        echo '<td>' . $student->first_name . '</td>';
+        echo '<td>' . $student->last_name . '</td>';
+        echo '<td>' . $student->nickname . '</td>';
+        echo '<td>' . $student->date_of_birth . '</td>';
+        echo '<td>' . $student->mark . '</td>';
+        echo "<td><a title='edit_" . $student['id'] . "' href='form.php?id=" . $student['id'] . "'>Edit</a></td>";
+        echo "<td><a title='delete_" . $student['id'] . "' href='process.php?delete=yes&id=" . $student['id'] . "'>Delete</a></td>";
+        echo '</tr>';
+      }
+      ?>
+      </tbody>
+    </table>
   </body>
 </html>
