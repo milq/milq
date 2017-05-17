f407536d1915a02cace741d5ccde3d1153d2a600
Manuel Ignacio López Quintero <miloqu@gmail.com>
2017-05-17 11:07:23 +0200

    Update index.php

--- learn/crud/sql/index.php
+++ learn/crud/sql/index.php
@@ -11,41 +11,43 @@
     <h1>Students</h1>
 	
     <a href='form.php'>Insert a new student</a><br /><br />
-	
-    <?php
-    require_once('connect_db.php');
+
+    <table title='student_table'>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>First name</th>
+          <th>Last name</th>
+          <th>Nickname</th>
+          <th>Date of birth</th>
+          <th>Mark</th>
+          <th>Edit</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>  
+      <?php
+      require_once('connect_db.php');
   
-    $sql = 'SELECT * FROM students';
-    $sth = $dbh->prepare($sql);
-    $sth->execute();       			
-    $rows = $sth->fetchAll();
+      $sql = 'SELECT * FROM students';
+      $sth = $dbh->prepare($sql);
+      $sth->execute();       			
+      $rows = $sth->fetchAll();
 
-    echo '<table>';
-    echo '<tr>';
-    echo '<th>ID</th>';
-    echo '<th>First name</th>';
-    echo '<th>Last name</th>';
-    echo '<th>Nickname</th>';
-    echo '<th>Date of birth</th>';
-    echo '<th>Mark</th>';
-    echo '<th>Edit</th>';
-    echo '<th>Delete</th>';
-    echo '</tr>';
-
-    foreach ($rows as &$row) {
-	  echo '<tr>';
-      echo '<td>' . $row['id'] . '</td>';
-      echo '<td>' . $row['first_name'] . '</td>';
-      echo '<td>' . $row['last_name'] . '</td>';
-      echo '<td>' . $row['nickname'] . '</td>';
-      echo '<td>' . $row['date_of_birth'] . '</td>';
-      echo '<td>' . $row['mark'] . '</td>';
-      echo '<td><a href=\'form.php?id=' . $row['id'] . '\'>Edit</a></td>';
-      echo '<td><a href=\'process.php?delete=yes&id=' . $row['id'] . '\'>Delete</a></td>';
-      echo '</tr>';
-    }
-
-    echo '</table>';
-    ?>
+      foreach ($rows as &$row) {
+        echo '<tr>';
+        echo '<td>' . $row['id'] . '</td>';
+        echo '<td>' . $row['first_name'] . '</td>';
+        echo '<td>' . $row['last_name'] . '</td>';
+        echo '<td>' . $row['nickname'] . '</td>';
+        echo '<td>' . $row['date_of_birth'] . '</td>';
+        echo '<td>' . $row['mark'] . '</td>';
+        echo '<td><a href=\'form.php?id=' . $row['id'] . '\'>Edit</a></td>';
+        echo '<td><a href=\'process.php?delete=yes&id=' . $row['id'] . '\'>Delete</a></td>';
+        echo '</tr>';
+      }
+      ?>
+      </tbody>
+    </table>
   </body>
 </html>
