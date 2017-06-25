3fe894edbe43aedb7665fa63f4e2a3a8670406eb
Manuel Ignacio López Quintero <miloqu@gmail.com>
2017-06-25 18:51:52 +0200

    Update install-numix-xfce-debian.sh

--- scripts/bash/install-numix-xfce-debian.sh
+++ scripts/bash/install-numix-xfce-debian.sh
@@ -1,1 +1,30 @@
+####################################
+# SET UP NUMIX ON DEBIAN WITH XFCE #
+####################################
 
+# | THIS SCRIPT IS TESTED CORRECTLY ON  |
+# |-------------------------------------|
+# | OS             | Test | Last test   |
+# |----------------|------|-------------|
+# | Debian 9.0     | OK   | 25 Jun 2017 |
+
+
+# 1. KEEP DEBIAN UP TO DATE
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get -y autoremove
+
+
+# 2. INSTALL NUMIX THEME AND NUMIX ICONS
+
+sudo apt-get install -y numix-gtk-theme
+sudo apt-get install -y numix-icon-theme
+
+
+# 3. ACTIVATE NUMIX'S THEME, WM, AND ICONS IN XFCE
+
+xfconf-query -c xsettings -p /Net/ThemeName -s 'Numix'
+xfconf-query -c xfwm4 -p /general/theme -s 'Numix'
+xfconf-query -c xsettings -p /Net/IconThemeName -s 'Numix-Circle'
