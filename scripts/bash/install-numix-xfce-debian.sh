af83733d79b1f32b49dc1eb303d260a0acd07270
Manuel Ignacio López Quintero <miloqu@gmail.com>
2017-06-25 21:21:54 +0200

    Update install-numix-xfce-debian.sh

--- scripts/bash/install-numix-xfce-debian.sh
+++ scripts/bash/install-numix-xfce-debian.sh
@@ -30,7 +30,7 @@ xfconf-query -c xsettings -p /Net/ThemeName -s 'Numix'
 xfconf-query -c xfwm4 -p /general/theme -s 'Numix'
 xfconf-query -c xsettings -p /Net/IconThemeName -s 'Numix-Circle'
 
-xfconf-query -c xfwm4 -p /general -s 'O|HMC'
+xfconf-query -c xfwm4 -p /general/button_layout -s 'O|HMC'
 
 xfconf-query -c xsettings -p /Xft/Antialias -s 1
 xfconf-query -c xsettings -p /Xft/DPI -s -1
