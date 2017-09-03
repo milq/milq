f88a869c1dd972f50c0c8827cb8da23e191ef9fe
Manuel Ignacio López Quintero <miloqu@gmail.com>
2017-09-03 20:16:45 +0200

    Update install-numix-xfce-debian.sh

--- scripts/bash/install-numix-xfce-debian.sh
+++ scripts/bash/install-numix-xfce-debian.sh
@@ -91,3 +91,5 @@ xfconf-query -c xsettings -p /Xft/RGBA -s 'rgb'
 xfconf-query -n -c xsettings -p /Xft/DPI -t int -s -1
 
 xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -s '/usr/local/share/images/debian_background.png'
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-show -s true
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-style -s 0
