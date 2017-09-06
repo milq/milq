5f52e906918f6e82edbdf2982865125fed1f9dba
Manuel Ignacio López Quintero <miloqu@gmail.com>
2017-09-06 20:22:09 +0200

    Update install-numix-xfce-debian.sh

--- scripts/bash/install-numix-xfce-debian.sh
+++ scripts/bash/install-numix-xfce-debian.sh
@@ -109,3 +109,5 @@ echo 'ColorForeground=#d4e1e2' >> ~/.config/xfce4/terminal/terminalrc
 echo 'ColorCursor=#93a1a1' >> ~/.config/xfce4/terminal/terminalrc
 echo 'TabActivityColor=#dc322f' >> ~/.config/xfce4/terminal/terminalrc
 echo 'ColorPalette=#073642;#dc322f;#859900;#b58900;#268bd2;#d33682;#2aa198;#eee8d5;#002b36;#cb4b16;#586e75;#657b83;#5e858b;#6c71c4;#93a1a1;#fdf6e3' >> ~/.config/xfce4/terminal/terminalrc
+
+
