07f2a29884a45c559c90c449462a19614f46bedc
Manuel Ignacio López Quintero <miloqu@gmail.com>
2018-01-17 09:52:40 +0100

    adding content0

--- learn/prog/user_input/user_input.py
+++ learn/prog/user_input/user_input.py
@@ -1,13 +1,20 @@
+# Example of execution: python3 user_input.py -3.4 'Hello, world!' 6
+
 import sys
  
 a = sys.argv[0]
 b = sys.argv[1]
 c = sys.argv[2]
+d = sys.argv[3]
 
-print('Script name:', sys.argv[0])
-print('First argument:', sys.argv[1])
-print('Second argument:', sys.argv[2])
-print('Third argument:', sys.argv[3])
+print('Script name:', a)
+print(' ')
+print('First argument:', b)
+print('Second argument:', c)
+print('Third argument:', d)
+print(' ')
+print('All arguments are string. E.g. type of first argument:', type(b))
+print(' ')
 
 total = len(sys.argv)
 print ('The total numbers of arguments passed to the script:', total)
