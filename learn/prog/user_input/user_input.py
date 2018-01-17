4e70d13bd9165459463cb5a855b248012f2b6e16
Manuel Ignacio López Quintero <miloqu@gmail.com>
2018-01-17 09:36:04 +0100

    adding content

--- learn/prog/user_input/user_input.py
+++ learn/prog/user_input/user_input.py
@@ -1,15 +1,16 @@
 import sys
  
-total = len(sys.argv)
-
-cmdargs = str(sys.argv)
-
-print ('The total numbers of arguments passed to the script:', total)
-
-print ("Args list: %s " % cmdargs)
-
-# Pharsing argument one by one 
+a = sys.argv[0]
+b = sys.argv[1]
+c = sys.argv[2]
 
 print('Script name:', sys.argv[0])
-print('First argument', sys.argv[1])
-print('Second argument', sys.argv[2])
+print('First argument:', sys.argv[1])
+print('Second argument:', sys.argv[2])
+print('Third argument:', sys.argv[3])
+
+total = len(sys.argv)
+print ('The total numbers of arguments passed to the script:', total)
+
+cmdargs = str(sys.argv)
+print ("Arguments list: %s " % cmdargs)
