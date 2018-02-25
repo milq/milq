0904830f951abbac0ca92388ba901c7491662262
Acebedo Ruzafa, Mateo <acrumateo@gmail.com>
2018-02-25 18:17:12 +0100

    Update 05.ps1

--- learn/prog/05/05.ps1
+++ learn/prog/05/05.ps1
@@ -1,3 +1,37 @@
+#Prints 'Hello, world!'
+
+function hello {
+   echo "Hello, world"
+}
+
+hello
+
+
+# Returns the square of a number
+# @param {Number} x - Number to be squared.
+# @returns {Number} y - Square of x
+
+function square($x)
+{
+    $y = $x * $x;
+    return $y;
+}
+
+square 10
+
+
+# Returns the sum of a and b
+# @param {Number} a - First number.
+# @param {Number} b - Second number.
+# @returns {Number} sum - Sum of a and b
+
+function sum($a, $b)
+{
+    $sum = $a + $b;
+    return $sum;
+}
+
+sum 10 5
 
 
 # FUNCTIONS
