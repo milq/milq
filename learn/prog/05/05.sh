c53e9659305412002ca9ff4095fd2d0d946f3d3e
Sergio Martin Lopez <33958751+smxrlxp@users.noreply.github.com>
2018-02-26 13:31:58 +0100

    Update 05.sh

--- learn/prog/05/05.sh
+++ learn/prog/05/05.sh
@@ -1,3 +1,60 @@
 #!/bin/bash
 
 # FUNCTIONS
+
+
+# Prints 'Hello, world!'
+
+function hello() 
+{
+    echo "Hello, world!"
+}
+
+
+
+# Returns the square of a number
+# @param {Number} x - Number to be squared.
+# @returns {Number} y - Square of x
+
+function square()
+{
+    let y=$x*$x
+    echo $y
+}
+
+
+
+# Returns the sum of a and b
+# @param {Number} a - First number.
+# @param {Number} b - Second number.
+# @returns {Number} sum - Sum of a and b
+ 
+function sum()
+{
+    let sum=$a+$b
+    echo $sum
+}
+
+
+
+
+
+
+
+
+
+
+## CALLING FUNCTIONS
+
+hello
+
+
+x=2
+sqr=$(square)
+echo "The square of $x is $sqr" 
+
+
+a=$sqr
+b=3
+s=$(sum)
+echo "The sum of $a and $b is $s"
