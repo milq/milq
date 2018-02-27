eda9e1249d3ec555f354bff0143d6f2e3a2d154b
Alejandro Ordóñez Caballero <33958855+alejandroorca@users.noreply.github.com>
2018-02-27 19:35:03 +0100

    Update 05.sh

--- learn/prog/05/05.sh
+++ learn/prog/05/05.sh
@@ -1,60 +1,53 @@
+# FUNCIONES
 #!/bin/bash
+# Existen dos tipos, con function o sin él
 
-# FUNCTIONS
-
-
-# Prints 'Hello, world!'
-
-function hello() 
-{
-    echo "Hello, world!"
+function suma(){
+c=$(expr $1 + $2)
+return $c
 }
 
-
-
-# Returns the square of a number
-# @param {Number} x - Number to be squared.
-# @returns {Number} y - Square of x
-
-function square()
-{
-    let y=$x*$x
-    echo $y
+function function suma(){
+c=$(expr $1 + $2)
+return $c
 }
 
+# De llamado
 
-
-# Returns the sum of a and b
-# @param {Number} a - First number.
-# @param {Number} b - Second number.
-# @returns {Number} sum - Sum of a and b
- 
-function sum()
-{
-    let sum=$a+$b
-    echo $sum
+funcion() { 
+    comandos 
 }
 
+funcion p1 p2 p3
 
+#Referencias indirectas
 
+#Permiten definir variables cuyo contenido es el nombre de otra variable:
 
+a=letra 
+letra=z 
+# Referencia directa 
+echo "a = $a"    # a = letra 
+# Referencia indirecta 
+eval a=\$$a 
+echo "Ahora a = $a"    # Ahora a = z
 
+#Paso de parámetros
 
+$ cat funcion1.sh 
+#!/bin/bash 
+funcion1() 
+{ 
+    echo "Parámetros pasados a la función: $*" 
+    echo "Parámetro 1: $1" 
+    echo "Parámetro 2: $2" 
+} 
+# Programa principal 
+funcion1 "hola" "que tal estás" adios 
+$ 
+$ bash funcion1.sh 
+#Parámetros pasados a la función: hola que tal estás adios 
+#Parámetro 1: hola 
+#Parámetro 2: que tal estás
 
-
-
-
-## CALLING FUNCTIONS
-
-hello
-
-
-x=2
-sqr=$(square)
-echo "The square of $x is $sqr" 
-
-
-a=$sqr
-b=3
-s=$(sum)
-echo "The sum of $a and $b is $s"
+return
