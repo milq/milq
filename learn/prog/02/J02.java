5efb0328a500cbe154feabbc9833fbb2c123f7c4
Manuel Ignacio López Quintero <miloqu@gmail.com>
2018-03-17 00:44:40 +0100

    Testing Java

---
+++ learn/prog/02/J02.java
@@ -0,0 +1,11 @@
+
+
+public class J02 {
+  public static void main(String[] args) {
+ 
+   String[] food = {"oranges","strawberries","lemons"};
+ 
+//	System.out.println(Arrays.toString(food));
+
+  }
+}
