76846f157befce52e6c3f24b481f61608a790655
Manuel Ignacio López Quintero <miloqu@gmail.com>
2018-04-18 18:50:49 +0200

    adding content

--- examples/php/form/process.php
+++ examples/php/form/process.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <html xmlns='http://www.w3.org/1999/xhtml' lang='en'>
   <head>
     <meta charset='utf-8' />
@@ -6,16 +5,27 @@
   </head>
 
   <body>
-    <h1>Basic form - Output data</h1>
+    <h1>Basic form - Processing data</h1>
     <?php
       $name = $_POST['name'];
       $age = $_POST['age'];
       $gender = $_POST['gender'];
+      $hobbies_array = [];
+
+      if (isset($_POST['hobbies'])) {
+        $hobbies_array = $_POST['hobbies'];
+      }
     ?>
 
-    <p>Your name is <?php echo $name ?>.</p>
-    <p>You are <?php echo $age ?> years old.</p>
-    <p>Your gender is <?php echo $gender ?>.</p>
+    <p>Your name is <?php echo $name; ?>.</p>
+    <p>You are <?php echo $age; ?> years old.</p>
+    <p>Your gender is <?php echo $gender; ?>.</p>
+    <p>Your hobbies are:</p>
+    <ul>
+    <?php for($i = 0; $i < count($hobbies_array); $i++) { ?>
+      <li><?php echo $hobbies_array[$i]; ?></li>
+    <?php } ?>
+    </ul>
 
   </body>
 </html>
