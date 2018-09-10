f974b2090ae90be0560a5f83da355debf4981e80
Manuel Ignacio López Quintero <miloqu@gmail.com>
2018-09-10 17:58:25 +0200

    Update install-additional-gui-educational-software.sh

--- scripts/bash/install-additional-gui-educational-software.sh
+++ scripts/bash/install-additional-gui-educational-software.sh
@@ -11,4 +11,10 @@ sudo apt-get -y autoremove
 sudo apt-get install -y geogebra
 sudo apt-get install -y gperiodic
 sudo apt-get install -y scratch
-sudo apt-get install -y epoptes
+sudo apt-get install -y epoptes-client
+
+# LOCK PANEL, DESKTOP AND APPAREANCE
+
+# sudo cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml /etc/xdg/xfce4/xfconf/xfce-perchannel-xml
+# sudo nano /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
+# Change '<channel name="xfce4-panel" version="1.0">' to '<channel name="xfce4-panel" version="1.0" locked="*" unlocked="root">'
