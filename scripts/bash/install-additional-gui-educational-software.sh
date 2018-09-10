897db48dd1982633f7388d9f75330594d0921fd0
Manuel Ignacio López Quintero <miloqu@gmail.com>
2018-09-10 19:10:48 +0200

    Update install-additional-gui-educational-software.sh

--- scripts/bash/install-additional-gui-educational-software.sh
+++ scripts/bash/install-additional-gui-educational-software.sh
@@ -16,5 +16,16 @@ sudo apt-get install -y epoptes-client
 # LOCK PANEL, DESKTOP AND APPAREANCE
 
 # sudo cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml /etc/xdg/xfce4/xfconf/xfce-perchannel-xml
+# sudo cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml /etc/xdg/xfce4/xfconf/xfce-perchannel-xml
+# sudo cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml /etc/xdg/xfce4/xfconf/xfce-perchannel-xml
+# sudo cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml /etc/xdg/xfce4/xfconf/xfce-perchannel-xml
+
 # sudo nano /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
+# sudo nano /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
+# sudo nano /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+# sudo nano /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
+
 # Change '<channel name="xfce4-panel" version="1.0">' to '<channel name="xfce4-panel" version="1.0" locked="*" unlocked="root">'
+# Change '<channel name="xfce4-desktop" version="1.0">' to '<channel name="xfce4-desktop" version="1.0" locked="*" unlocked="root">'
+# Change '<channel name="xfwm4" version="1.0">' to '<channel name="xfwm4" version="1.0" locked="*" unlocked="root">'
+# Change '<channel name="xsettings" version="1.0">' to '<channel name="xsettings" version="1.0" locked="*" unlocked="root">'
