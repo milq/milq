815ef86d1fc0baf63992d746f229d96cd8454d34
Manuel Ignacio López Quintero <miloqu@gmail.com>
2018-09-10 20:28:19 +0200

    Update install-additional-gui-educational-software.sh

--- scripts/bash/install-additional-gui-educational-software.sh
+++ scripts/bash/install-additional-gui-educational-software.sh
@@ -1,3 +1,11 @@
+# PREREQUISITES
+
+# Use a Xfce Debian-based distribution and:
+
+# a) Execute install-numix-xfce-debian.sh (optional: use your ~./debian_bg.png).
+# b) Execute install-additional-cli-software.sh.
+# c) Execute install-additional-gui-software.sh.
+
 # 1. KEEP DEBIAN UP TO DATE
 
 sudo apt-get -y update
@@ -5,15 +13,14 @@ sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
 sudo apt-get -y autoremove
 
-
-# 2. DOWNLOAD FONTS
+# 2. DOWNLOAD SOFTWARE
 
 sudo apt-get install -y geogebra
 sudo apt-get install -y gperiodic
 sudo apt-get install -y scratch
-sudo apt-get install -y epoptes-client
+# sudo apt-get install -y epoptes-client
 
-# LOCK PANEL, DESKTOP AND APPAREANCE
+# 3. LOCK PANEL, DESKTOP AND APPAREANCE
 
 # sudo cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml /etc/xdg/xfce4/xfconf/xfce-perchannel-xml
 # sudo cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml /etc/xdg/xfce4/xfconf/xfce-perchannel-xml
@@ -29,3 +36,20 @@ sudo apt-get install -y epoptes-client
 # Change '<channel name="xfce4-desktop" ver...' to '<channel name="xfce4-desktop" version="1.0" locked="*" unlocked="root">'
 # Change '<channel name="xfwm4" ver...' to '<channel name="xfwm4" version="1.0" locked="*" unlocked="root">'
 # Change '<channel name="xsettings" ver...' to '<channel name="xsettings" version="1.0" locked="*" unlocked="root">'
+
+
+# 4. CONFIGURE AND SET UP
+
+# sudo chmod 555 ~./Desktop
+
+# Install last version of LibreOffice
+
+# Configure Geany as you wish.
+# Configure Gimp as you wish.
+
+# Set Chromium to open PDF files by default.
+# Set Ristretto to open JPG, PNG, GIF files by default.
+# Set Geany to open text files by default.
+
+
+# 5. CLONE WITH CLONEZILLA
