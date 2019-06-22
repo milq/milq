487ec5be8bc747c50a5f06f56a13e32cf9ca88e1
Manuel Ignacio López Quintero <miloqu@gmail.com>
2019-06-22 19:51:37 +0200

    adding content

--- scripts/bash/install-opencv.sh
+++ scripts/bash/install-opencv.sh
@@ -22,9 +22,9 @@ OPENCV_VERSION='4.1.0'    # Version to be installed
 # 1. KEEP UBUNTU OR DEBIAN UP TO DATE
 
 sudo apt-get -y update
-# sudo apt-get -y upgrade       # Uncomment this line to install the newest versions of all packages currently installed
-# sudo apt-get -y dist-upgrade  # Uncomment this line to, in addition to 'upgrade', handles changing dependencies with new versions of packages
-# sudo apt-get -y autoremove    # Uncomment this line to remove packages that are now no longer needed
+# sudo apt-get -y upgrade       # Uncomment to install new versions of packages currently installed
+# sudo apt-get -y dist-upgrade  # Uncomment to handle changing dependencies with new vers. of pack.
+# sudo apt-get -y autoremove    # Uncomment to remove packages that are now no longer needed
 
 
 # 2. INSTALL THE DEPENDENCIES
@@ -32,14 +32,17 @@ sudo apt-get -y update
 # Build tools:
 sudo apt-get install -y build-essential cmake
 
-# GUI (if you want to use GTK instead of Qt, replace 'qt5-default' with 'libgtkglext1-dev' and remove '-DWITH_QT=ON' option in CMake):
+# GUI (if you want GTK, change 'qt5-default' to 'libgtkglext1-dev' and remove '-DWITH_QT=ON'):
 sudo apt-get install -y qt5-default libvtk6-dev
 
 # Media I/O:
-sudo apt-get install -y zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev libjasper-dev libopenexr-dev libgdal-dev
+sudo apt-get install -y zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev libjasper-dev \
+                        libopenexr-dev libgdal-dev
 
 # Video I/O:
-sudo apt-get install -y libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev
+sudo apt-get install -y libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev \
+                        libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm \
+                        libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev
 
 # Parallelism and linear algebra libraries:
 sudo apt-get install -y libtbb-dev libeigen3-dev
@@ -58,14 +61,12 @@ sudo apt-get install -y doxygen
 
 sudo apt-get install -y unzip wget
 wget https://github.com/opencv/opencv/archive/${OPENCV_VERSION}.zip
-unzip ${OPENCV_VERSION}.zip
-rm ${OPENCV_VERSION}.zip
+unzip ${OPENCV_VERSION}.zip && rm ${OPENCV_VERSION}.zip
 mv opencv-${OPENCV_VERSION} OpenCV
-cd OpenCV
-mkdir build
-cd build
-cmake -DWITH_QT=ON -DWITH_OPENGL=ON -DFORCE_VTK=ON -DWITH_TBB=ON -DWITH_GDAL=ON -DWITH_XINE=ON -DBUILD_EXAMPLES=ON -DENABLE_PRECOMPILED_HEADERS=OFF ..
-make -j$(nproc)
+cd OpenCV && mkdir build && cd build
+cmake -DWITH_QT=ON -DWITH_OPENGL=ON -DFORCE_VTK=ON -DWITH_TBB=ON -DWITH_GDAL=ON \
+      -DWITH_XINE=ON -DBUILD_EXAMPLES=ON -DENABLE_PRECOMPILED_HEADERS=OFF ..
+make -j4
 sudo make install
 sudo ldconfig
 
