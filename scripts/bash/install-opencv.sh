d513c9189f096cb09265d2358b57c02f63579ead
Manuel Ignacio López Quintero <miloqu@gmail.com>
2019-09-28 17:44:09 +0200

    Adding content

--- scripts/bash/install-opencv.sh
+++ scripts/bash/install-opencv.sh
@@ -5,8 +5,8 @@
 # -------------------------------------------------------------------- |
 #                       SCRIPT OPTIONS                                 |
 # ---------------------------------------------------------------------|
-OPENCV_VERSION='4.1.1'       # Version to be installed                 |
-OPENCV_CONTRIB='YES'         # Install OpenCV's extra modules (YES/NO) |
+OPENCV_VERSION='4.1.1'       # Version to be installed
+OPENCV_CONTRIB='NO'          # Install OpenCV's extra modules (YES/NO)
 # -------------------------------------------------------------------- |
 
 # |          THIS SCRIPT IS TESTED CORRECTLY ON          |
