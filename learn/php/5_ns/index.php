9daba5bd6eb8688de2636cca093326cf5611d2c1
Manuel Ignacio López Quintero <miloqu@gmail.com>
2020-03-08 17:16:42 +0100

    Update PHP course

--- learn/php/5_ns/index.php
+++ learn/php/5_ns/index.php
@@ -1,4 +1,6 @@
 <?php
+// 1. TRY YOURSELF: run 'php index.php', what happens? Why?
+
 require_once('bar/A.php');
 require_once('bar/B.php');
 require_once('foo/C.php');
@@ -8,13 +10,13 @@ use J\X;
 $x = new X();
 echo $x->get_greeting();
 
-// TRY YOURSELF: uncomment these lines and run 'php index.php', what happens? Why?
+// 2. TRY YOURSELF: uncomment these lines and run 'php index.php', what happens? Why?
 // use K\X;
 //
 // $x = new X();
 // echo $x->get_variable();
 
-// TRY YOURSELF: change to 'L\say_hello;' and run 'php index.php', what happens? Why?
+// 3. TRY YOURSELF: change to 'L\say_hello;' and run 'php index.php', what happens? Why?
 use function K\say_hello;
 
 echo say_hello();
