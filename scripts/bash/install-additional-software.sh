43447c0c87e6d742297de093e3c6e691bd7c9f28
Manuel Ignacio López Quintero <miloqu@gmail.com>
2022-07-09 18:52:02 +0200

    Update install-additional-software.sh

--- scripts/bash/install-additional-software.sh
+++ scripts/bash/install-additional-software.sh
@@ -6,6 +6,7 @@
 # |------------------------------------|
 # | OS             | Test | Last test  |
 # |----------------|------|------------|
+# | Debian 11.3    | OK   | 9 Jul 2022 |
 # | Debian 11.0    | OK   | 8 Oct 2021 |
 # | Debian 10.1    | OK   | 7 Oct 2019 |
 
@@ -25,12 +26,14 @@ sudo apt-get install -y p7zip p7zip-full unrar-free unzip   # FILE ARCHIVERS
 sudo apt-get install -y htop lshw wget locate               # UTILITIES
 sudo apt-get install -y screen                              # TERMINAL MULTIPLEXER
 sudo apt-get install -y nano vim                            # TEXT EDITORS
-sudo apt-get install -y git subversion                      # VCS
+sudo apt-get install -y git                                 # VCS
 sudo apt-get install -y gdisk                               # PARTITION TOOL
 sudo apt-get install -y pdftk                               # PDF MANIPULATION
 sudo apt-get install -y ffmpeg                              # VIDEO MANIPULATION
-sudo apt-get install -y default-jdk                         # JAVA DEVELOPMENT KIT (JDK)
-sudo apt-get install -y wavemon                             # NET
+sudo apt-get install -y wavemon                             # WIRELESS DEVICE MONITORING
+sudo apt-get install -y default-jre                         # JAVA RUNTIME ENVIRONMENT
+
+# sudo apt-get install -y default-jdk                       # JAVA DEVELOPMENT KIT (optional)
 # sudo apt-get install -y tesseract-ocr tesseract-ocr-eng   # OCR (optional)
 
 
@@ -45,12 +48,12 @@ sudo apt-get install -y transmission-gtk                    # BITTORRENT CLIENT
 sudo apt-get install -y galculator                          # SCIENTIFIC CALCULATOR
 sudo apt-get install -y vlc                                 # VIDEO AND AUDIO PLAYER
 sudo apt-get install -y pavucontrol                         # VOLUME CONTROL
-
 sudo apt-get install -y geany                               # TEXT EDITOR
-sudo apt-get install -y blender gimp imagemagick inkscape   # GRAPHICS EDITORS
+sudo apt-get install -y blender gimp inkscape               # GRAPHICS EDITORS
 sudo apt-get install -y audacity                            # AUDIO EDITOR
-sudo apt-get install -y openshot                            # VIDEO EDITOR
 sudo apt-get install -y filezilla                           # FTP/FTPS/SFTP CLIENT
-sudo apt-get install -y kazam                               # SCREENCAST (optional)
+sudo apt-get install -y kazam                               # SCREENCAST
+
 # sudo apt-get install -y libreoffice                       # OFFICE (optional, not last version)
 # sudo apt-get install -y texlive-full texmaker             # LATEX (optional, heavy package)
+# sudo apt-get install -y imagemagick                       # IMAGE MANIPULATION PROGRAM (optional)
